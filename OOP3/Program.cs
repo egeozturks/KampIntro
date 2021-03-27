aafbdfa17e1fdc72e6f665ee89da54d9657e5cb0
Ege OZTURK <egeozturk1994@gmail.com>
2021-03-27 12:38:18 +0100

    OOP2 and OOP3 created, Inheritance and Infertace

---
+++ OOP3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+
+            //BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(new EsnafKrediManager(), databaseLoggerService);
+
+            //BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(esnafKrediManager, new SmsLoggerService());
+
+            //BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(esnafKrediManager, new List<ILoggerService> {new DatabaseLoggerService(), new SmsLoggerService() });
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new SmsLoggerService(), new DatabaseLoggerService() };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+
+
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager, konutKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+        }
+    }
+}
